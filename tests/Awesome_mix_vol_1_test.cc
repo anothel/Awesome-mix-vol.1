152a5ce4ad42ef5345e2862713846ff1df4ed5a7
pilkyu <anothel1@naver.com>
2022-01-12 17:33:07 +0900

    - 테스트 내용 수정  -- Append 테스트 완료

--- tests/Awesome_mix_vol_1_test.cc
+++ tests/Awesome_mix_vol_1_test.cc
@@ -5,7 +5,7 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include "include/afxstr.h"
+#include "include/atlstr.h"
 #include "include/b.h"
 
 TEST(Inheriting_from_a_template, test1) { AAA<int> aaa(1); }
@@ -22,7 +22,8 @@ TEST(CStringTest1, test1) {
   // a.Append(simplestring1);
 
   a.Append(cpsz);
-  a.Append(cpsz, strlen(cpsz));  
+  a.Append(cpsz, strlen(cpsz));
+  printf("[jpk] a: %s \n", a.GetBuffer());
   } catch (int i) {
 printf("[jpk] i: %d \n", i);
   } catch (char * s) {
@@ -34,19 +35,19 @@ printf("[jpk] %s(%d) \n", __FILE__, __LINE__);
 
 TEST(CStringTest1, test2) { 
   CString a;
-  const char* cpsz = "This is test 2";
+  // const char* cpsz = "This is test 2";
 
-  awesome_ATL::CSimpleStringT<char> simplestring2(cpsz, ::AfxGetStringManager());
-  awesome_ATL::CSimpleStringT<char> simplestring3(cpsz, strlen(cpsz), ::AfxGetStringManager());
-  a.Append(simplestring2);  
+  // awesome_ATL::CSimpleStringT<char> simplestring2(cpsz, ::AfxGetStringManager());
+  // awesome_ATL::CSimpleStringT<char> simplestring3(cpsz, strlen(cpsz), ::AfxGetStringManager());
+  // a.Append(simplestring2);
 }
 
-TEST(CStringTest1, test3) { 
-  CString a;
-  const char* cpsz = "This is test 3";
+// TEST(CStringTest1, test3) { 
+//   CString a;
+//   // const char* cpsz = "This is test 3";
 
-  awesome_ATL::CSimpleStringT<char> simplestring3(cpsz, strlen(cpsz), ::AfxGetStringManager());
-  a.Append(simplestring3);  
-}
+//   // awesome_ATL::CSimpleStringT<char> simplestring3(cpsz, strlen(cpsz), ::AfxGetStringManager());
+//   // a.Append(simplestring3);
+// }
 
-TEST(ByteStringTest2, RectangleSize_false) {}
+// TEST(ByteStringTest2, RectangleSize_false) {}
