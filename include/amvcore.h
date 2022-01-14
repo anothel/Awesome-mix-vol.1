9bf87b627ebb19d666d7826e168b48ff6bfed921
pilkyu <anothel1@naver.com>
2022-01-14 11:54:51 +0900

    - 함수 템플릿 추가

--- include/amvcore.h
+++ include/amvcore.h
@@ -1,8 +1,36 @@
 #include "include/amvdefine.h"
 
+namespace {
+
 /////////////////////////////////////////////////////////////////////////////
 // Verify that a null-terminated string points to valid memory
-inline BOOL AmvIsValidString(_In_reads_z_(nMaxLength) LPCSTR psz,
+inline BOOL AmvIsValidString(_In_reads_z_(nMaxLength) const char* psz,
                              _In_ size_t nMaxLength = UINT_MAX) {
   return (psz != NULL);
 }
+
+template <typename _CharType>
+inline _CharType* AmvCharNext(_In_ const _CharType* p) throw() {
+  // Too expensive to check separately here
+  AMVASSUME(p != NULL);
+  return const_cast<_CharType*>(p + 1);
+}
+
+template <typename CharType>
+inline const CharType* AmvstrchrT(_In_z_ const CharType* p,
+                                  _In_ CharType ch) throw() {
+  ATLASSERT(p != NULL);
+  if (p == NULL) {
+    return NULL;
+  }
+  while (*p != 0) {
+    if (*p == ch) {
+      return p;
+    }
+    p = AtlCharNext(p);
+  }
+  // strchr for '\0' should succeed - the while loop terminates
+  //*p == 0, but ch also == 0, so NULL terminator address is returned
+  return (*p == ch) ? p : NULL;
+}
+}  // namespace
