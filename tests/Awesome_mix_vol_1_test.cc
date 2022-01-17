9bb5ac05f62a5a9c1747845bb79892522abf09c7
pilkyu <anothel1@naver.com>
2022-01-17 15:28:11 +0900

    - 테스트 코드 변경

--- tests/Awesome_mix_vol_1_test.cc
+++ tests/Awesome_mix_vol_1_test.cc
@@ -26,113 +26,109 @@ TEST(BStringT, construct) {
 
   std::string sTest("This is test | This is test | AA");
   BString bstring2(sTest.c_str());
- 
-  // unsigned char uszTest[] = {0x00, 0x01, 0x02, 0x02};
-  // BString bstring3(uszTest, sizeof(uszTest));
+  bstring1 = bstring2;
+
+  unsigned char uszTest[] = {0x00, 0x01, 0x02, 0x02};
+  BString bstring3(uszTest, sizeof(uszTest));
 }
 
-// TEST(BStringT, func) {
-//   try {
-//     std::string sTest("This is test | This is test | AA");
-//     BString bstring(sTest.c_str());
+TEST(BStringT, func) {
+  try {
+    std::string sTest("This is test | This is test | AA");
+    BString bstring(sTest.c_str());
 
-//     // ASSERT_EQ(bstring.Compare(szTestStrAA), S_OK);
+    ASSERT_EQ(bstring.Compare(szTestStrAA), S_OK);
 
-//     // ASSERT_EQ(bstring.CompareNoCase(sztestStringAA), S_OK);
+    ASSERT_EQ(bstring.CompareNoCase(sztestStringAA), S_OK);
 
-//     // bstring.Delete(4, 4);
-//     // ASSERT_EQ(memcmp(bstring, szTestStrDel, strlen(szTestStrDel)), S_OK);
+    bstring.Delete(4, 4);
+    ASSERT_EQ(memcmp(bstring, szTestStrDel, strlen(szTestStrDel)), S_OK);
 
-//     // bstring = sTest.c_str();
-//     // bstring.Insert(0, "^");
-//     // ASSERT_EQ(std::string(bstring), std::string("^" + sTest));
+    bstring = sTest.c_str();
+    bstring.Insert(0, "^");
+    ASSERT_EQ(std::string(bstring), std::string("^" + sTest));
 
-//     // bstring.Insert(0, static_cast<char>(45));
-//     // ASSERT_EQ(std::string(bstring), std::string("-^" + sTest));
+    bstring.Insert(0, static_cast<char>(45));
+    ASSERT_EQ(std::string(bstring), std::string("-^" + sTest));
 
-//     // bstring.Replace("^", "");
-//     // ASSERT_EQ(std::string(bstring), std::string("-" + sTest));
+    bstring.Replace("^", "");
+    ASSERT_EQ(std::string(bstring), std::string("-" + sTest));
 
-//     // bstring.Replace(static_cast<char>(45), static_cast<char>(95));
-//     // ASSERT_EQ(std::string(bstring), std::string("_" + sTest));
+    bstring.Replace(static_cast<char>(45), static_cast<char>(95));
+    ASSERT_EQ(std::string(bstring), std::string("_" + sTest));
 
-//     // bstring.Remove(static_cast<char>(95));
-//     // ASSERT_EQ(std::string(bstring), std::string(sTest));
+    bstring.Remove(static_cast<char>(95));
+    ASSERT_EQ(std::string(bstring), std::string(sTest));
 
-//     // printf("[jpk] %s \n", bstring.GetBuffer());
+    printf("[jpk] %s \n", bstring.GetBuffer());
 
 
  
-// // BString str("ABC|DEF|EEE|");
-// // BString token;
-// // int pos = 0;
-// // while ((token  = str.Tokenize("|", pos)) != "")
-// //     printf("%s\n", token.GetBuffer());
+    // bstring.Find(static_cast<char>(0), 0);
+    // bstring.Find("This", 0);
 
-//     // bstring.Tokenize();
-//     // bstring.Find();
-//     // bstring.FindOneOf();
-//     // bstring.ReverseFind();
-//     // bstring.MakeUpper();
-//     // bstring.MakeLower();
-//     // bstring.MakeReverse();
-//     // bstring.TrimRight();
-//     // bstring.TrimLeft();
-//     // bstring.Trim();
-//     // bstring.Mid();
-//     // bstring.Right();
-//     // bstring.Left();
-//     // bstring.SpanIncluding();
-//     // bstring.SpanExcluding();
-//   } catch (...) {
-//   }
-// }
+    // bstring.FindOneOf();
+    // bstring.ReverseFind();
+    // bstring.MakeUpper();
+    // bstring.MakeLower();
+    // bstring.MakeReverse();
+    // bstring.TrimRight();
+    // bstring.TrimLeft();
+    // bstring.Trim();
+    // bstring.Mid();
+    // bstring.Right();
+    // bstring.Left();
+    // bstring.SpanIncluding();
+    // bstring.SpanExcluding();
+  } catch (...) {
+  }
+}
 
-// TEST(_Func_UnitTest, CSimpleStringT) {
-//   try {
-//     BString bstring;
+TEST(_Func_UnitTest, CSimpleStringT) {
+  try {
+    BString bstring;
 
-//     bstring.Append(pszTestStr);
-//     bstring.Append(pszTestStr, strlen(pszTestStr));
-//     ASSERT_EQ(memcmp(bstring, szTestStr, strlen(szTestStr)), S_OK);
+    bstring.Append(pszTestStr);
+    bstring.Append(pszTestStr, strlen(pszTestStr));
+    ASSERT_EQ(memcmp(bstring, szTestStr, strlen(szTestStr)), S_OK);
 
-//     bstring.AppendChar('A');
-//     bstring.AppendChar('A');
-//     ASSERT_EQ(memcmp(bstring, szTestStrAA, strlen(szTestStrAA)), S_OK);
+    bstring.AppendChar('A');
+    bstring.AppendChar('A');
+    ASSERT_EQ(memcmp(bstring, szTestStrAA, strlen(szTestStrAA)), S_OK);
 
-//     bstring.CopyChars(szDest, szSrc, strlen(szSrc));
-//     ASSERT_EQ(memcmp(szDest, szSrc, strlen(szSrc)), S_OK);
+    bstring.CopyChars(szDest, szSrc, strlen(szSrc));
+    ASSERT_EQ(memcmp(szDest, szSrc, strlen(szSrc)), S_OK);
 
-//     bstring.CopyChars(szDest, sizeof(szDest), szSrc, strlen(szSrc));
-//     ASSERT_EQ(memcmp(szDest, szSrc, strlen(szSrc)), S_OK);
+    bstring.CopyChars(szDest, sizeof(szDest), szSrc, strlen(szSrc));
+    ASSERT_EQ(memcmp(szDest, szSrc, strlen(szSrc)), S_OK);
 
-//     bstring.CopyCharsOverlapped(szDest, szSrc, strlen(szSrc));
-//     ASSERT_EQ(memcmp(szDest, szSrc, strlen(szSrc)), S_OK);
+    bstring.CopyCharsOverlapped(szDest, szSrc, strlen(szSrc));
+    ASSERT_EQ(memcmp(szDest, szSrc, strlen(szSrc)), S_OK);
  
-//     bstring.Empty();
-//     ASSERT_EQ(bstring.GetLength(), 0);
+    bstring.Empty();
+    ASSERT_EQ(bstring.GetLength(), 0);
 
-//     // bstring.FreeExtra();
-//     // bstring.GetAllocLength();
-//     // bstring.GetAt();
-//     // bstring.GetBuffer();
-//     // bstring.GetBufferSetLength();
-//     // bstring.GetLength();
-//     // bstring.GetString();
-//     // bstring.IsEmpty();
-//     // bstring.UnlockBuffer();
-//     // bstring.Preallocate();
-//     // bstring.ReleaseBuffer();
-//     // bstring.ReleaseBufferSetLength();
-//     // bstring.Truncate();
-//     // bstring.SetAt();
-//     // bstring.SetManager();
-//     // bstring.SetString();
-//     // bstring.Concatenate();
-//   } catch (...) {
-//   }
-// }
+    // bstring.FreeExtra();
+    // bstring.GetAllocLength();
+    // bstring.GetAt();
+    // bstring.GetBuffer();
+    // bstring.GetBufferSetLength();
+    // bstring.GetLength();
+    // bstring.GetString();
+    // bstring.IsEmpty();
+    // bstring.UnlockBuffer();
+    // bstring.Preallocate();
+    // bstring.ReleaseBuffer();
+    // bstring.ReleaseBufferSetLength();
+    // bstring.Truncate();
+    // bstring.SetAt();
+    // bstring.SetManager();
+    // bstring.SetString();
+    // bstring.Concatenate();
+  } catch (...) {
+  }
+}
 
-// TEST(BStringTest1, test3) { BString bstring; }
+TEST(BStringTest1, test3) { BString bstring; }
 
-// TEST(ByteStringTest2, RectangleSize_false) {}
+TEST(ByteStringTest2, RectangleSize_false) {}
